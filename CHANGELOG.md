87373b8b6aac3a2f018098742a8950614daf3f13
Aliaksandr Pashkevich <apashkevich3@alfabank.ru>
2024-01-12 01:04:43 +0300

    fix(): test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,3 @@
-# Changelog
-
-All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
-
 ## 12.0.0 (2023-12-29)
 
 ## 11.1.0 (2023-12-29)
--- bin/prettyfy-changelog-beta.sh
+++ bin/prettyfy-changelog-beta.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
-changelog_file="./bin/CHANG.md"
-#changelog_file="CHANG.md"
-temp_file_ext=".temp"
-regex="^#+\s*[0-9]+\.[0-9]+\.[0-9]+ \([0-9]{4}-[0-9]{2}-[0-9]{2}\)"
-start_line=$(grep -n -m 1 -E "$regex" "$changelog_file" | cut -d ':' -f 1)
-# second_line=$(grep -n -m 2 -E "$regex" "$changelog_file" | tail -n 1 | cut -d ':' -f 1)
+changelog_file="CHANGELOG.md";
+new_line="
+";
+separator="
+- ";
 
-function rmiTempFile {
-  rm "$changelog_file$temp_file_ext"
-}
-
-function fillChanges {
-  if [ -z "$1" ]
-  then
-      echo "Значение $2 не установлено"
-  elif [ $# == 3 ]
-    # Тут будет логика по заполнению файла ↓
-   then sed
-     sed -i"$temp_file_ext" "\$a\\
-     $2\\
-     $1" "$3"
-
-
-    #then info="$2$1"; sed -i"$temp_file_ext" -e "a\\
-    #        $info" "$3" # && sed -i"$temp_file_ext" 's/\//\n- /g' "$3"
-
-    # Тут будет логика по заполнению файла ↑
+function generateTitle {
+  if [[ $SEMANTIC_VERSION == "patch" ]];
+    then caption="###"
+      elif [[ $SEMANTIC_VERSION == "minor" ]];
+        then caption="##"
+      elif [[ $SEMANTIC_VERSION == "major" ]];
+            then caption="#"
   fi
+
+  echo "$caption $VERSION ($(date +%d-%m-%Y))"
 }
 
-# sed -i"$temp_file_ext" "$((start_line+1)),$((second_line-1))d" "$changelog_file" && rmiTempFile
+function generateChangelogInfo {
+      local ChangelogInfo="";
 
-# sed -i"$temp_file_ext" "$((start_line))G" "$changelog_file" && rmiTempFile
+      if [[ ${#FEATURES} -gt 0 ]];
+      then ChangelogInfo+="$new_line### Features${FEATURES//\//$separator}"
+      fi
 
-#touch "changelog$temp_file_ext"
-fillChanges "$FEATURES_ENV" "### Features" "changelog$temp_file_ext"
-fillChanges "$BREAKING_ENV" "### Bug Fixes" "changelog$temp_file_ext"
-fillChanges "$BUGFIX_ENV" "### ⚠ BREAKING CHANGES" "changelog$temp_file_ext"
-#rm "changelog$temp_file_ext"
-#rm "changelog$temp_file_ext$temp_file_ext"
+      if [ ${#BUGFIXES} -gt 0 ];
+        then ChangelogInfo+="$new_line### Bug Fixes${BUGFIXES//\//$separator}"
+      fi
+
+      if [ ${#BREAKING_CHANGES} -gt 0 ];
+        then ChangelogInfo+="$new_line### BREAKING CHANGES${BREAKING_CHANGES//\//$separator}"
+      fi
+
+      echo "$ChangelogInfo$new_line"
+}
+
+Title=$(generateTitle)
+Changelog=$(generateChangelogInfo)
+
+fullChangelogInfo="$Title
+$Changelog
+"
+
+echo "$fullChangelogInfo" | cat - "$changelog_file" > temp && mv temp "$changelog_file"
