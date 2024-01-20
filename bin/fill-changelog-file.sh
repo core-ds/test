51278df4e6f1f4545d280996435c17bdf799a891
Aliaksandr Pashkevich <apashkevich3@alfabank.ru>
2024-01-21 01:45:52 +0300

    fix(): test

--- bin/fill-changelog-file.sh
+++ bin/fill-changelog-file.sh
@@ -79,7 +79,19 @@ function postReleaseMessage {
   )
 
   release_id=$(echo "$response" | jq -r '.id')
-    echo "Release ID: $release_id"
+
+  FILENAME="release_$VERSION"
+
+  zip -r "$FILENAME" .publish/
+
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $BOT_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "Content-Type: application/octet-stream" \
+    "https://uploads.github.com/repos/$REPOSITORY/releases/$release_id/assets?name=$FILENAME" \
+    --data-binary "@$FILENAME"
 }
 
 
