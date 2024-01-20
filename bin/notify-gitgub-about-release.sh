dfe050a6d5090bf6593c8d1d953f442380a962b7
Aliaksandr Pashkevich <apashkevich3@alfabank.ru>
2024-01-20 16:42:09 +0300

    fix(): test

---
+++ bin/notify-gitgub-about-release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Отправляет запрос о релизе в github release API
+function postReleaseMessage {
+  echo "$BOT_TOKEN"
+  echo "$VERSION"
+  echo "$BRANCH"
+  echo "$CHANGELOG"
+
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $BOT_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -d "{
+      \"tag_name\": \"$VERSION\",
+      \"target_commitish\": \"$BRANCH\",
+      \"name\": \"$VERSION\",
+      \"body\": \"$CHANGELOG\",
+      \"draft\": false,
+      \"prerelease\": false,
+      \"generate_release_notes\": false
+    }" \
+    https://api.github.com/core-ds/test/releases
+}
+
+
+postReleaseMessage
