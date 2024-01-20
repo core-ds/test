2aa8ac9c1c9987023ffb7b7b766b09a06c1b903a
Aliaksandr Pashkevich <apashkevich3@alfabank.ru>
2024-01-20 16:15:31 +0300

    fix(): test

--- bin/fill-changelog-file.sh
+++ bin/fill-changelog-file.sh
@@ -72,7 +72,7 @@ function postReleaseMessage {
       \"prerelease\": false,
       \"generate_release_notes\": false
     }" \
-    https://api.github.com/repos/core-ds/test/releases
+    "https://api.github.com/$REPOSITORY/releases"
 }
 
 
