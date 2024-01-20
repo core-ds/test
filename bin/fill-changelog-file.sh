fa12ba9af49ed45da5b293955eff61d0268029bf
Aliaksandr Pashkevich <apashkevich3@alfabank.ru>
2024-01-20 16:11:40 +0300

    fix(): test

--- bin/fill-changelog-file.sh
+++ bin/fill-changelog-file.sh
@@ -58,21 +58,23 @@ $Changelog
 
 # Отправляет запрос о релизе в github release API
 function postReleaseMessage {
-curl -L \
-  -X POST \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $BOT_TOKEN" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  -d "{
-    'tag_name': $VERSION,
-    'target_commitish': $BRANCH,
-    'name': $VERSION,
-    'body': $Changelog,
-    'draft': false,
-    'prerelease': false,
-    'generate_release_notes': false }" \
-  https://api.github.com/repos/core-ds/test/releases
-};
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $BOT_TOKEN" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -d "{
+      \"tag_name\": \"$VERSION\",
+      \"target_commitish\": \"$BRANCH\",
+      \"name\": \"$VERSION\",
+      \"body\": \"$Changelog\",
+      \"draft\": false,
+      \"prerelease\": false,
+      \"generate_release_notes\": false
+    }" \
+    https://api.github.com/repos/core-ds/test/releases
+}
+
 
 postReleaseMessage
 
